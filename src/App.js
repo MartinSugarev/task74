1947d558a5a8baae60e859036ddba28c3b953bda
Martin <mdsugarev@gmail.com>
2022-01-09 18:26:33 +0200

    q

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import "./App.css";
+import React, { useState, useMemo, useEffect } from 'react'
 
 function App() {
+
+const [number, SetNumber] = useState('');
+//const [isNumber, SetIsNumber] = useState(false)
+
+useEffect(() => {
+  console.log(boo);
+  
+}, [number])
+
+
+const boo = useMemo(() => {
+  let r = /^\d+$/;
+  let isN = number.match(r);
+  return isN
+}, [number])
+
+function reg(e){
+ //let r = /^\d+$/;
+ //if(e.target.value.match(r)){
+   SetNumber(number => e.target.value);
+  // SetIsNumber(true);
+ //}
+ //else{
+  //SetNumber(number => e.target.value);
+  //SetIsNumber(false);
+ //}
+}
+
+
+
+
   return (
     <div className="App">
      <div className="control has-icons-right">
@@ -8,10 +40,13 @@ function App() {
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
-        />
+          value={number}
+          onChange={(e) => {
+            reg(e)
+          }}
+           />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
+          <i className={boo ? "fas fa-check" :"fas fa-times"} />
         </span> 
       </div>
     </div>
