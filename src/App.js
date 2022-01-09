2d0643608e2e2f512b847987b71d375c28c70daf
Martin <mdsugarev@gmail.com>
2022-01-09 21:03:22 +0200

    p

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import "./App.css";
-import React, { useState, useMemo } from 'react'
+import React, { useState, useMemo, useEffect } from 'react'
 
 function App() {
 
-const [text, Settext] = useState('');
+const [state, SetState] = useState({text: ''});
 //const [isNumber, SetIsNumber] = useState(false)
+
 
 const boo = useMemo(() => {
   let r = /^\d+$/;
   
-  if(text.match(r)){
+  if(state.text.match(r)){
    return true
   }else{
     return false
   }
   
-}, [text])
+}, [state.text])
 
   return (
     <div className="App">
      <div className="control has-icons-right">
         <input
-         value={text}
+         value={state.text}
           className="input is-large"
           type="text"
           placeholder="Enter number..."
           onChange={(e) => {
-            Settext( text => e.target.value)
+            SetState(prevValue => {
+              return {...prevValue, text: e.target.value}
+            })
           }}
            />
         <span className="icon is-small is-right">
